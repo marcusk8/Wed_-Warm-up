c6c39600518405b5c0f2b98669dabeac782f4d4a
Marcus Kueh <marcus.k@scclc.net>
2020-07-22 17:34:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# num = input("Give me a number: ")
+# num = int(num)
+
+# if num > 10: 
+#   print("greater than 10")
+# elif num < 10: 
+#   print("less than 10")
+# else:
+#   print("equal to 10")
+
+num = input("Give me a positive or negative number: ")
+num = int(num)
+
+
+if num > 0:
+  print("Positive Number")
+elif num < 0:
+  print("Negative Number")
+else:
+  print("Equal To Zero")
+
+  
